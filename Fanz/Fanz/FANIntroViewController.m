ef77d4a0055b8ba938edbb73e864daf5b25f4348
Ankur Oberoi <aoberoi@gmail.com>
2013-04-28 07:17:09 -0400

    works for first login

--- Fanz/Fanz/FANIntroViewController.m
+++ Fanz/Fanz/FANIntroViewController.m
@@ -12,7 +12,7 @@
 
 @interface FANIntroViewController ()
 
-- (void)updateView;
+- (void)handleOpenSessionCompletionWithSession:(FBSession *)session state:(FBSessionState)status error:(NSError *)error;
 
 @end
 
@@ -37,26 +37,17 @@
     self.backgroundImageView.image = [[UIImage imageNamed:@"intro_background"] resizableImageWithCapInsets:UIEdgeInsetsMake(24.0, 24.0, 24.0, 24.0)
                                                                                               resizingMode:UIImageResizingModeStretch];
     
-    //[self updateView];
-    
-    //FANAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-    
     if (![FBSession activeSession].isOpen) {
-        // create a fresh session object
-        //appDelegate.facebookSession = [[FBSession alloc] init];
     
         if ([FBSession activeSession].state == FBSessionStateCreatedTokenLoaded) {
-            // even though we had a cached token, we need to login to make the session usable
-            //[appDelegate.facebookSession openWithCompletionHandler:^(FBSession *session,
-            //                                                 FBSessionState status,
-            //                                                 NSError *error) {
             [FBSession openActiveSessionWithReadPermissions:@[@"email"]
                                                allowLoginUI:NO
                                           completionHandler:^(FBSession *session, FBSessionState status, NSError *error) {
-                // we recurse here, in order to update buttons and labels
-                [self updateView];
+                [self handleOpenSessionCompletionWithSession:session state:status error:error];
             }];
         }
+    } else {
+        [self handleOpenSessionCompletionWithSession:[FBSession activeSession] state:[FBSession activeSession].state error:nil];
     }
     
     
@@ -69,56 +60,37 @@
 }
 
 - (IBAction)loginPressed:(id)sender {
-    //NSArray *permissions = [NSArray arrayWithObjects:@"email", nil];
-    
-    //FANAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
     
     if ([FBSession activeSession].isOpen) {
         NSLog(@"facebook session was already logged in");
         return;
     } else {
-        if ([FBSession activeSession].state != FBSessionStateCreated) {
-            // Create a new, logged out session.
-            //appDelegate.facebookSession = [[FBSession alloc] init];
-             NSLog(@"facebook session is cached on button press but wasn't on load");
-        }
-        
         // if the session isn't open, let's open it now and present the login UX to the user
-        //[appDelegate.facebookSession openWithCompletionHandler:^(FBSession *session,
-        //                                                 FBSessionState status,
-        //                                                 NSError *error) {
         [FBSession openActiveSessionWithReadPermissions:@[@"email"]
                                            allowLoginUI:YES
                                       completionHandler:^(FBSession *session, FBSessionState status, NSError *error) {
-            
-            if (error) {
-                NSLog(@"error after opening facebook session: %@", error.description);
-                return;
-            }
-            
-            NSLog(@"facebook session state: %d", status);
-            
-            // and here we make sure to update our UX according to the new session state
-            [self updateView];
+            [self handleOpenSessionCompletionWithSession:session state:status error:error];
         }];
     }
-    
-//    [FBSession openActiveSessionWithReadPermissions:permissions
-//                                       allowLoginUI:YES
-//                                  completionHandler:^(FBSession *session, FBSessionState status, NSError *error) {
-//                                      appDelegate.facebookSession = session;
-//                                      /* handle success + failure in block */
-//                                      NSLog(@"Login success");
-//                                  }];
 }
 
-- (void) updateView
+- (void)handleOpenSessionCompletionWithSession:(FBSession *)session state:(FBSessionState)status error:(NSError *)error
 {
-    //FANAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-    if ([FBSession activeSession].isOpen) {
-        NSLog(@"facebook session is open");
-    } else {
-        NSLog(@"facebook session is not open");
+    if (error) {
+        NSLog(@"error after opening facebook session: %@", error.description);
+        return;
+    }
+    
+    switch (status) {
+        case FBSessionStateOpen:
+            [self performSegueWithIdentifier:@"loggedIn" sender:self];
+            break;
+        case FBSessionStateClosedLoginFailed:
+            NSLog(@"facebook session login failed");
+            break;
+        default:
+            NSLog(@"facebook session transitioned into some other state");
+            break;
     }
 }
 
